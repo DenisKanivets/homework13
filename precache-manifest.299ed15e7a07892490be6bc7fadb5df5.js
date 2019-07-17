a34279549236808113c18bac682fe8741ad0fb82
Denis Kanivets <denis.kanivets@ukr.net>
2019-07-17 17:31:20 +0300

    Updates

---
+++ precache-manifest.299ed15e7a07892490be6bc7fadb5df5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a0c40c99cd6b860a3a53ac1406dd7b",
+    "url": "/homework13/index.html"
+  },
+  {
+    "revision": "cb21ddac716584089910",
+    "url": "/homework13/static/css/main.02e0e8b2.chunk.css"
+  },
+  {
+    "revision": "8795d4af043ddfe37042",
+    "url": "/homework13/static/js/2.7cb9f535.chunk.js"
+  },
+  {
+    "revision": "cb21ddac716584089910",
+    "url": "/homework13/static/js/main.f06dfb24.chunk.js"
+  },
+  {
+    "revision": "0b1c78762f538c76d529",
+    "url": "/homework13/static/js/runtime~main.0920fd24.js"
+  }
+]);
